f0572e6693447c18f62d379b2bbe6e57338b3784
omanoma <omanoma14@gmail.com>
2025-09-02 04:47:43 +0100

    fix bug

--- Assets/Script/SceneManagers.cs
+++ Assets/Script/SceneManagers.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -21,7 +21,7 @@ public class SceneManagers : MonoBehaviour
 
     private SceneObject currentScene;
 
-    int indextextScene = 0;//
+    int indextextScene = 0;
 
     public void startGame()
     {
